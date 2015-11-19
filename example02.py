759303c85d3716b944fa43e4fb78802d38acfb1c
bennyxx22 <it58070079@office365.it.kmitl.ac.th>
2015-11-19 18:19:50 +0700

    lastest edition

---
+++ example02.py
@@ -0,0 +1,88 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+#Get data from csv file
+data = open('Real_Final_database_02.csv')
+alldata = data.readlines()
+listdata = []
+for i in alldata:
+    listdata.append(i.strip().split(','))
+
+#Seperate each list to display in graph
+country = input()
+types = input()
+year = []
+affect = []
+damage = []
+death =[]
+for j in listdata:
+    if j[0] == country and j[2] == types:
+        year.append(int(j[1]))
+        affect.append(int(j[3]))
+        damage.append(int(j[4]))
+        death.append(int(j[5]))
+    
+#Put the data of graph
+trace1 = go.Scatter(
+    x = year,
+    y = affect,
+    name = 'Total Affected'
+)
+trace2 = go.Scatter(
+    x = year,
+    y = damage,
+    name = 'Total Damage',
+    yaxis='y2'
+)
+trace3 = go.Scatter(
+    x = year,
+    y = death,
+    name = 'Total Death',
+    yaxis='y3'
+)
+
+#arrange and colour
+data = [trace1, trace2, trace3]
+layout = go.Layout(
+    title = 'ASIAN DISASTER',
+    width = 3000,
+    xaxis=dict(
+        domain=[0.3, 0.7]
+    ),
+    yaxis=dict(
+        title = 'Total Affected',
+        titlefont=dict(
+            color='#1f77b4'
+        ),
+        tickfont=dict(
+            color='#1f77b4'
+        )
+    ),
+    yaxis2=dict(
+        title = 'Total Damage',
+        titlefont=dict(
+            color='#ff7f0e'
+        ),
+        tickfont=dict(
+            color='#ff7f0e'
+        ),
+        anchor='free',
+        overlaying='y',
+        side='left',
+        position=0.15
+    ),
+    yaxis3=dict(
+        title = 'Total Death',
+        titlefont=dict(
+            color='#d62728'
+        ),
+        tickfont=dict(
+            color='#d62728'
+        ),
+        anchor='x',
+        overlaying='y',
+        side='right'
+    ),
+)
+fig = go.Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='bennyy')
