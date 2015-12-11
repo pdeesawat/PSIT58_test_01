d476b8a5f4eac20c5f4209d7c9d24dba3e84fbd6
S8070124 <58070124@kmitl.ac.th>
2015-12-11 12:53:19 +0700

    mslaysia

--- Real_3_Graph/Malaysia/total_affect_Malaysia.py
+++ Real_3_Graph/Malaysia/total_affect_Malaysia.py
@@ -35,8 +35,7 @@ layout = go.Layout(
     
     yaxis=dict(title='Total Affected',
         titlefont=dict(color='#eb0056'),
-        tickfont=dict(color='#eb0056')
-               ),
+        tickfont=dict(color='#eb0056')),
     
     autosize=True,
     plot_bgcolor=["rgba(0, 0, 0, 0)"],
