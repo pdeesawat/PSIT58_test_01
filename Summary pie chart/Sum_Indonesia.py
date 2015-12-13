3bf86c4598d2cb37560c24e66d977b3a6820eee7
pdeesawat <s8070090@kmitl.ac.th>
2015-12-13 22:20:45 +0700

    Edit Summary of Cambodia and Indonesia

--- Summary pie chart/Sum_Indonesia.py
+++ Summary pie chart/Sum_Indonesia.py
@@ -1,19 +1,19 @@
 import plotly.plotly as py
 
-#Get data from csv and split it
+"""Get data from csv and split it"""
 data = open('Real_Final_database_02.csv')
 alldata = data.readlines()
 listdata = []
 for ix in alldata:
     listdata.append(ix.strip().split(','))
 
-#seperate data in each type of disaster.
+"""Seperate data in each type of disaster."""
 all_disaster = {'Drought':0, 'Flood':0, 'Storm':0, 'Epidemic':0, 'Earthquake':0}
 for iy in listdata:
         if iy[0] == 'Indonesia' and iy[2] in all_disaster:
             all_disaster[iy[2]] += 1
 
-#calculate the each type for make an average.
+"""Calculate each type for make an average."""
 total = sum(all_disaster.values())
 average = []
 for iz in all_disaster:
@@ -21,7 +21,7 @@ for iz in all_disaster:
 label = [i for i in all_disaster]
 value = [all_disaster[j] for j in label]
 
-#apprerance
+"""Apprerance"""
 make_circle = {"data": [{"values":value,"labels":label,
 "name": "Average", "hoverinfo":"label+percent+name", "hole": 0.39, "type": "pie"}], 
 "layout": {"title":"Indonesia's Average Disaster from 2000 to 2014", "annotations": [{"font": {"size": 20},
