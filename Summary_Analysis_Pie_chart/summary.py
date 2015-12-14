1dc835d97b619e5f352fca98cfc882033c21e1d1
bennyxx22 <it58070079@office365.it.kmitl.ac.th>
2015-12-14 14:04:08 +0700

    Create Summary Analysis of project

---
+++ Summary_Analysis_Pie_chart/summary.py
@@ -0,0 +1,34 @@
+import plotly.plotly as py
+
+#Get data from csv and split it
+data = open('database_v0.csv')
+alldata = data.readlines()
+listdata = []
+for ix in alldata:
+    listdata.append(ix.strip().split(','))
+
+#Get values of disaster in each country    
+allcountry = ['Indonesia', 'Lao P Dem Rep', 'Malaysia', 'Philippines', 'Singapore',
+           'Thailand', 'Cambodia', 'Viet Nam', 'Brunei', 'Myanmar']
+country = ['IDN', 'LAO', 'MYS', 'PHL', 'SGP', 'THA', 'KHM', 'VNM', 'NBD', 'MMR']
+listvalues = []
+for iy in country:
+    count = 0
+    for iz in listdata:
+        if iz[2] == iy:
+            count += 1
+    listvalues.append(count)
+    
+#Calculate to percent
+summ = sum(listvalues)
+value = []
+for ia in listvalues:
+    num = (ia/summ)*100
+    value.append("%.2f" % num)
+listvalues = [float(ib) for ib in value]
+
+#Apperence
+pie_chart = {'data':[{'labels':allcountry, 'values':listvalues, 'type': 'pie'}],
+    'layout': {'title': 'Asian disaster'}}
+
+url = py.plot(pie_chart)
